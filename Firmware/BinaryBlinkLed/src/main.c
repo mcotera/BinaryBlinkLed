0499390356cdec717d38f93db1d4e5384fc6c1f7
mcotera <cotera.miguel.03@gmail.com>
2017-09-04 12:17:52 -0500

    firmware system updated

--- Firmware/BinaryBlinkLed/src/main.c
+++ Firmware/BinaryBlinkLed/src/main.c
@@ -8,36 +8,72 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define	DDR_LED01	DDB3
-#define	DDR_LED02	DDB4
+#define	DDR_LED01	DDRB
+#define	DDR_LED02	DDRB
+#define	DDR_LED03	DDRB
+
+#define	PORT_LED01	PORTB
+#define	PORT_LED02	PORTB
+#define	PORT_LED03	PORTB
+
 #define	LED01		PB3
 #define	LED02		PB4
+#define	LED03		PB0
 
 int main(void){
 
 	// Set up Port B pin 3 and 4 mode to output
-	DDRB |= (1 << DDR_LED01);
-	DDRB |= (1 << DDR_LED02);
+	DDR_LED01 |= (1 << LED01);
+	DDR_LED02 |= (1 << LED02);
+	DDR_LED03 |= (1 << LED03);
 
 	for (;;) {
 		// 0 binary
-		PORTB &= ~(1 << PB3);
-		PORTB &= ~(1 << PB4);
+		PORT_LED01 &= ~(1 << LED01);
+		PORT_LED02 &= ~(1 << LED02);
+		PORT_LED03 &= ~(1 << LED03);
 		_delay_ms(500);
 
 		// 1 binary
-		PORTB |= (1 << PB3);
-		PORTB &= ~(1 << PB4);
+		PORT_LED01 |= (1 << LED01);
+		PORT_LED02 &= ~(1 << LED02);
+		PORT_LED03 &= ~(1 << LED03);
 		_delay_ms(500);
 
 		// 2 binary
-		PORTB &= ~(1 << PB3);
-		PORTB |= (1 << PB4);
+		PORT_LED01 &= ~(1 << LED01);
+		PORT_LED02 |= (1 << LED02);
+		PORT_LED03 &= ~(1 << LED03);
 		_delay_ms(500);
 
-		// 1 binary
-		PORTB |= (1 << PB3);
-		PORTB |= (1 << PB4);
+		// 3 binary
+		PORT_LED01 |= (1 << LED01);
+		PORT_LED02 |= (1 << LED02);
+		PORT_LED03 &= ~(1 << LED03);
+		_delay_ms(500);
+
+		// 4 binary
+		PORT_LED01 &= ~(1 << LED01);
+		PORT_LED02 &= ~(1 << LED02);
+		PORT_LED03 |= (1 << LED03);
+		_delay_ms(500);
+
+		// 5 binary
+		PORT_LED01 |= (1 << LED01);
+		PORT_LED02 &= ~(1 << LED02);
+		PORT_LED03 |= (1 << LED03);
+		_delay_ms(500);
+
+		// 6 binary
+		PORT_LED01 &= ~(1 << LED01);
+		PORT_LED02 |= (1 << LED02);
+		PORT_LED03 |= (1 << LED03);
+		_delay_ms(500);
+
+		// 7 binary
+		PORT_LED01 |= (1 << LED01);
+		PORT_LED02 |= (1 << LED02);
+		PORT_LED03 |= (1 << LED03);
 		_delay_ms(500);
 
 	}
